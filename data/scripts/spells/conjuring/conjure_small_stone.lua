f0cbe8b7c04091bc7d6c76d84e480310c1b9b333
jpinckney <jeffpinckney.90@gmail.com>
2021-10-21 14:13:03 -0600

    additional balancing

--- data/scripts/spells/conjuring/conjure_small_stone.lua
+++ data/scripts/spells/conjuring/conjure_small_stone.lua
@@ -1,7 +1,7 @@
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
-	return creature:conjureItem(0, 1294, 5, CONST_ME_MAGIC_BLUE)
+	return creature:conjureItem(0, 1294, 3, CONST_ME_MAGIC_BLUE)
 end
 
 spell:group("support")
@@ -10,8 +10,8 @@ spell:words("exevo grit")
 spell:cooldown(2 * 1000)
 spell:groupCooldown(2 * 1000)
 spell:level(30)
-spell:mana(125)
-spell:soul(1)
+spell:mana(75)
+spell:soul(4)
 spell:vocation("paladin;true", "royal paladin;true")
 spell:needLearn(false)
 spell:register()
